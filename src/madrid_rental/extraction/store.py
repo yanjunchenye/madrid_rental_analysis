f07b6fab348a68a9b4d41bdb223351e9f0c06933
yanjunchenye <yanjunchen69@gmail.com>
2026-06-24 16:50:18 +0200

    upsert terminado

--- src/madrid_rental/extraction/store.py
+++ src/madrid_rental/extraction/store.py
@@ -26,22 +26,56 @@ def save_anuncios(anuncios):
     with engine.begin() as conn:
         for a in anuncios:
             barrio_id = get_or_create_barrio(conn, a["neighborhood"])
+
+            existente = conn.execute(
+                text("SELECT id, precio FROM anuncios WHERE id_idealista = :id"),
+                {"id" : a["propertyCode"]}
+            ).fetchone()
             
-            conn.execute(
-                text("""
-                    INSERT INTO anuncios (id_idealista, barrio_id, precio, superficie, habitaciones, banos, url)
-                    VALUES (:id_idealista, :barrio_id, :precio, :superficie, :habitaciones, :banos, :url)
-                """),
-                {
-                    "id_idealista": a["propertyCode"],
-                    "barrio_id": barrio_id,
-                    "precio": a["price"],
-                    "superficie": a["size"],
-                    "habitaciones": a["rooms"],
-                    "banos": a["bathrooms"],
-                    "url": a["url"],
-                }
-            )
+            if existente is None:
+                nuevo_id = conn.execute(
+                    text("""
+                        INSERT INTO anuncios (id_idealista, barrio_id, precio, superficie, habitaciones, banos, url)
+                        VALUES (:id_idealista, :barrio_id, :precio, :superficie, :habitaciones, :banos, :url) RETURNING id
+                    """),
+                    {
+                        "id_idealista": a["propertyCode"],
+                        "barrio_id": barrio_id,
+                        "precio": a["price"],
+                        "superficie": a["size"],
+                        "habitaciones": a["rooms"],
+                        "banos": a["bathrooms"],
+                        "url": a["url"],
+                    }
+                ).fetchone()
+
+                conn.execute(
+                    text("""
+                        INSERT INTO historico_precios (anuncio_id, precio) VALUES (:anuncio_id, :precio)
+                     """),
+                     {
+                         "anuncio_id": nuevo_id[0],
+                         "precio": a["price"]
+                     }
+                )
+            else:
+                if existente[1] != a["price"]:
+                    conn.execute(
+                        text("""
+                            UPDATE anuncios SET precio = :precio_nuevo WHERE id = :id
+                        """),
+                        {"precio_nuevo" : a["price"], 
+                         "id" : existente[0]})
+                    conn.execute(
+                        text("""
+                            INSERT INTO historico_precios (anuncio_id, precio) VALUES (:anuncio_id, :precio) 
+                        """),
+                        {"anuncio_id": existente[0],
+                         "precio" : a["price"]}
+                    )
+                else:
+                    pass
+                    
 
 
 if __name__ == "__main__":
